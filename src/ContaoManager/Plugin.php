4e4e9970d4bdc74d3d71178a8f85ab42ea7c076f
Daniel Kiesel <icodr8@gmail.com>
2018-01-28 20:52:17 +0100

    optimized contao manager support

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,18 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Craffft\AccountmailBundle\ContaoManager;
 
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Craffft\AccountmailBundle\CraffftAccountmailBundle;
 use Craffft\TranslationFieldsBundle\CraffftTranslationFieldsBundle;
 
-class Plugin
+class Plugin implements BundlePluginInterface
 {
     public function getBundles(ParserInterface $parser)
     {
         return [
-            BundleConfig::create(CraffftTranslationFieldsBundle::class),
+            BundleConfig::create(CraffftAccountmailBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class, CraffftTranslationFieldsBundle::class])
+                ->setReplace(['accountmail']),
         ];
     }
 }
