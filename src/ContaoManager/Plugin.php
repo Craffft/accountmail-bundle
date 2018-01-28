eab1c831b7842d9ba1614de57d90c9989e199b28
Daniel Kiesel <icodr8@gmail.com>
2018-01-28 14:00:37 +0100

    added contao manager support

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -2,6 +2,17 @@
 
 namespace Craffft\AccountmailBundle\ContaoManager;
 
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Craffft\TranslationFieldsBundle\CraffftTranslationFieldsBundle;
+
 class Plugin
 {
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(CraffftTranslationFieldsBundle::class),
+        ];
+    }
 }
