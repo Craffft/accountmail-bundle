3b8c425c09cba12115c14bf6f271b814f3ea9acd
Daniel Kiesel <icodr8@gmail.com>
2018-01-30 18:22:38 +0100

    added runonce

---
+++ src/Resources/contao/config/runonce.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * This file is part of the Accountmail Bundle.
+ *
+ * (c) Daniel Kiesel <https://github.com/iCodr8>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$updater = new Craffft\AccountmailBundle\Util\Updater();
+$updater->addDefaultEmailContents();
